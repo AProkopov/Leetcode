309b309402902f48e788a889b7a1e40ec3e5338b
Anton Prokopov <asprokopov@domclick.ru>
2024-02-14 12:08:24 +0000

    MissingNumber with O(n) time complexity

--- src/MissingNumber.kt
+++ src/MissingNumber.kt
@@ -19,4 +19,16 @@ class MissingNumber {
 
         return -1
     }
+
+    fun missingNumberWithOnTimeComplexity(nums: IntArray): Int {
+        var indexSum = 0
+        var numSum = 0
+
+        nums.forEachIndexed {index, number ->
+            indexSum += index + 1
+            numSum += number
+        }
+
+        return indexSum - numSum
+    }
 }
