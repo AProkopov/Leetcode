9bdf7d1b34e7827c558ecae5d8b7b57365625228
Anton Prokopov <prokopov.anton@gmail.com>
2026-01-13 20:01:28 +0000

    SumTwo solved with O(n) by velocity

--- src/solutions_2026/TwoSum.kt
+++ src/solutions_2026/TwoSum.kt
@@ -3,36 +3,23 @@ package solutions_2026
 import java.io.IOException
 
 class TwoSum {
+    // O(n) both time and space
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        //TODO try solving that with HashMap
+        val map = HashMap<Int, Int>()
 
-        val diffList = mutableListOf<Int>()
-        var index_1 = -1
-        var index_2 = -1
-
-        for (i in 0 .. nums.lastIndex) {
+        for(i in 0 .. nums.lastIndex) {
             val diff = target - nums[i]
-            diffList.add(diff)
-        }
-
-        for (i in 0  .. nums.lastIndex) {
-            if (nums[i] + diffList[i] == target && diffList[i] >= 0) {
-                if (index_1 < 0) {
-                    index_1 = i
-                } else {
-                    index_2 = i
-                }
-
-                if (index_1 > 0 && index_2 > 0) return intArrayOf(index_1, index_2)
+            if (map.containsKey(diff)) {
+                return intArrayOf(map.get(diff)!!, i)
             }
+            map[nums[i]] = i
         }
 
-        throw IOException("no solution")
-
+        return intArrayOf()
     }
 
+    // O(n^2)
     fun twoSumX2(nums: IntArray, target: Int): IntArray {
-
         for(i in 0 until nums.lastIndex) {
             for(j in i + 1 .. nums.lastIndex) {
                 if (nums[i] + nums[j] == target) return intArrayOf(i, j)
