748a4bfea29fc748b3916fd42f461c20c1c9a0bd
Anton Prokopov <prokopov.anton@gmail.com>
2026-03-21 10:50:29 +0000

    Intersection of two linked lists - optimal solution

--- src/solutions_2026/IntersectionOfTwoLinkedLists.kt
+++ src/solutions_2026/IntersectionOfTwoLinkedLists.kt
@@ -16,9 +16,22 @@ class IntersectionOfTwoLinkedLists {
      * }
      */
 
+    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+        var a = headA
+        var b = headB
+
+        while (a != b) {
+            a = if (a != null) a.next else headB
+            b = if (b != null) b.next else headA
+        }
+
+        return a
+    }
+
+
     val nodes = mutableMapOf<ListNode, Int>()
 
-    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+    fun getIntersectionNodeSlow(headA: ListNode?, headB: ListNode?): ListNode? {
         headA?.let { goTroughNodeAndPutToMap(it) }
         return headB?.let { goTroughNodeAndPutToMap(it) }
     }
